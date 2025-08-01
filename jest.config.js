cc25672bba9dd95d4abd6a48323bec0f1998efd6
Soheil Sadeghbayan <liam@ramsalt.com>
2025-08-01 14:46:56 +0200

    config jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
+    roots: ['<rootDir>'],
     testMatch: [
       '**/__tests__/**/*.ts',
       '**/?(*.)+(spec|test).ts'
     ],
     collectCoverageFrom: [
-      'src/**/*.ts',
-      '!src/**/*.d.ts',
-      '!src/**/*.test.ts',
-      '!src/**/*.spec.ts'
+        "*.ts",
+      '**/*.ts',
+      '!**/*.d.ts',
+      '!**/*.test.ts',
+      '!**/*.spec.ts'
     ],
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov', 'html']
